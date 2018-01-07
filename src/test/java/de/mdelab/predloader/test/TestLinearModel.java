a2b25223b6447dafe1caffb1282889a734ac32a5
Christian Medeiros Adriano <christian.adriano@gmail.com>
2018-01-07 12:11:19 +0100

    Automated testing

--- src/test/java/de/mdelab/predloader/test/TestLinearModel.java
+++ src/test/java/de/mdelab/predloader/test/TestLinearModel.java
@@ -1,10 +1,13 @@
 package de.mdelab.predloader.test;
 
 import static org.junit.Assert.*;
+
+import org.jpmml.evaluator.InputField;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.mdelab.predloader.DataLoader;
@@ -12,91 +15,100 @@ import de.mdelab.predloader.RegressionModel;
 
 public class TestLinearModel {
 
-	private static String path = "..\\resources";
+	private static String path = "C://Users//chris//MachineLearning//ModelLoader//src//test//resources//";
 	private static String fileName = "Linear10K.csv";
-	
-	@Test
+
+	//@Test
 	public void testHardcoded() {
-		
+
 		Float allowedDeviation = new Float(0.1);
-		
-		
+
+
 		RegressionModel lrm = new de.mdelab.predloader.RegressionModel(
 				RegressionModel.path, RegressionModel.linear_pmml_fileName);
-			try {
-				lrm.loadModel();
-				System.out.println("Follow the model features:");
-				lrm.showModelFeatures();
-				
-				System.out.println("Allow deviation from actual value= "+allowedDeviation.toString());
-				
-				System.out.println("Running test");
-				Map<String,Double> userArguments = new LinkedHashMap<String,Double>();
-				userArguments.put("REQUIRED_INTERFACE", new Double(1) );
-				userArguments.put("PROVIDED_INTERFACE", new Double(1) );
-				userArguments.put("CRITICALITY", new Double(7) );
-				userArguments.put("RELIABILITY", new Double(0.5) );
-				userArguments.put("PMax", new Double(11.61201) );
-				userArguments.put("alpha", new Double(170.6764) );
-				
-				Double actual = new Double(7);
-				
-				Float predicted = lrm.pointPrediction(userArguments);
-				System.out.println("Predicted="+predicted+", Actual="+actual);
-				 
-				Double min = actual - allowedDeviation;
-				Double max = actual + allowedDeviation;				
-				assertTrue("Prediction" +predicted+ " is out of range for actual = "+ actual, predicted>= min && predicted <= max);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		try {
+			lrm.loadModel();
+			System.out.println("Follow the model features:");
+			lrm.showModelFeatures();
+
+			System.out.println("Allow deviation from actual value= "+allowedDeviation.toString());
+
+			System.out.println("Running test");
+			Map<String,Double> userArguments = new LinkedHashMap<String,Double>();
+			userArguments.put("REQUIRED_INTERFACE", new Double(1) );
+			userArguments.put("PROVIDED_INTERFACE", new Double(1) );
+			userArguments.put("CRITICALITY", new Double(7) );
+			userArguments.put("RELIABILITY", new Double(0.5) );
+			userArguments.put("PMax", new Double(11.61201) );
+			userArguments.put("alpha", new Double(170.6764) );
+
+			Double actual = new Double(7);
+
+			Float predicted = lrm.pointPrediction(userArguments);
+			System.out.println("Predicted="+predicted+", Actual="+actual);
+
+			Double min = actual - allowedDeviation;
+			Double max = actual + allowedDeviation;				
+			assertTrue("Prediction" +predicted+ " is out of range for actual = "+ actual, predicted>= min && predicted <= max);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	 
-	
-	
-	//@Test
+	}
+
+
+
+	@Test
 	public void testAutomated() {
-		
+
 		DataLoader dataLoader = new DataLoader(this.path,this.fileName);
 		ArrayList<LinkedHashMap<String,String>> tupleMapList = dataLoader.load();
-		
-		Float allowedDeviation = new Float(10);
-		
-		
+
+		Float allowedDeviation = new Float(0.15); //15% error
+		System.out.println("Allow deviation from actual value= "+allowedDeviation.toString());		
+
 		RegressionModel lrm = new de.mdelab.predloader.RegressionModel(RegressionModel.path, RegressionModel.linear_pmml_fileName);
-			try {
+		System.out.println("Running test");
+		try {
+			lrm.loadModel();
+			System.out.println("Follow the model features:");
+			lrm.showModelFeatures();
+
+			List<InputField> requiredModelFeatures = lrm.getActiveFields();
+			
+			//Read each line and run a test.
+			//for(LinkedHashMap<String,String> map: tupleMapList){
+			for(int i=0;i< 2;i++){
+				LinkedHashMap<String,String> map = tupleMapList.get(i);
+				Double actual = new Double(map.get("UTILITY_INCREASE"));
+				map.remove("UTILITY_INCREASE");
 				
-				lrm.loadModel();
-				System.out.println("Follow the model features:");
-				lrm.showModelFeatures();
-				
-				System.out.println("Allow deviation from actual value= "+allowedDeviation.toString());
-				
-				System.out.println("Running test");
 				Map<String,Double> userArguments = new LinkedHashMap<String,Double>();
-				userArguments.put("CRITICALITY", new Double(18) );
-				userArguments.put("CONNECTIVITY", new Double(4) );
-				userArguments.put("RELIABILITY", new Double(0.5) );
+				for(InputField field: requiredModelFeatures){
+					System.out.println("input field:" + field.getName().getValue());
+					System.out.println("value: "+ map.get(field.getName()));
+					userArguments.put(field.getName().getValue(), new Double(map.get(field.getName())) );
+				}
 				
-				Double actual = new Double(79.2);
 				
+
 				Float predicted = lrm.pointPrediction(userArguments);
 				System.out.println("Predicted="+predicted+", Actual="+actual);
-				 
+
 				Double min = actual - allowedDeviation;
 				Double max = actual + allowedDeviation;				
 				assertTrue("Prediction" +predicted+ " is out of range for actual = "+ actual, predicted>= min && predicted <= max);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
+
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	
-	
-	
-	
 	}
+
+
+
+
+}
 
 /* Data for testing
 FAILURE_NAME	AFFECTED_COMPONENT	CRITICALITY	UTILITY_INCREASE	CONNECTIVITY	RELIABILITY	IMPORTANCE	PROVIDED_INTERFACE	REQUIRED_INTERFACE	ADT	RULE	 PMax	alpha	REPLICA	REQUEST
@@ -104,6 +116,6 @@ CF3				Recomm. Item Filter		9			9				2				0.5			12			1					1					1.208279251	HwR
 CF3	 			Persistence Service		7			17.5			5				0.5			8			5					0					1.142992741	HwRedeployComponent	14.73108942	93.8030449	9	201
 CF1			 	Comment Item Filter		7			7				2				0.5			12			1					1					1.366570528	RestartComponent	13.4881317	158.2886289	13	495
 CF2	 			Region Item Filter		6			6				2				0.5			8			1					1					1.34484166	HwRedeployComponent	17.16943865	93.91547799	42	983
-*/
+ */
 
 
