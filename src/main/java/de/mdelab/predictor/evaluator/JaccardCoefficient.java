9fb158e5a0627b852ca5fe524946fd6c1417ac73
Christian Medeiros Adriano <christian.adriano@gmail.com>
2018-01-09 22:59:31 +0100

    implementing jaccard coefficient

---
+++ src/main/java/de/mdelab/predictor/evaluator/JaccardCoefficient.java
@@ -0,0 +1,42 @@
+package de.mdelab.predictor.evaluator;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Compute the Tanimoto coefficient between two lists.
+ * 
+ *  
+ * @author Christian M. Adriano
+ *
+ */
+public class JaccardCoefficient extends RankingMetric{
+
+	@Override
+	public Double compute(LinkedHashMap<Integer,String> mapOne, LinkedHashMap<Integer,String> mapTwo) {
+		
+		if(mapOne.size()!=mapTwo.size())
+			return null;
+		
+		double union = mapOne.size();
+		double intersecction = 0;
+		
+		for(Map.Entry<Integer,String> entry:mapOne.entrySet()){
+			String value = (String) entry.getValue();
+			Integer key = (Integer) entry.getKey();
+			String value2 = mapTwo.get(key);
+			if(value.matches(value2))
+				intersecction++;
+		}
+		
+		return intersecction/union;
+	}
+
+	public JaccardCoefficient(){}
+	
+	private int countSetSize
+	
+	
+	
+	
+}
