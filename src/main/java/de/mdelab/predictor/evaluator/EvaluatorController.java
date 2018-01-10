5d8e5a522ef0b8e9facd59e521adfce25b136329
Christian Medeiros Adriano <christian.adriano@gmail.com>
2018-01-10 14:36:13 +0100

    Refactoring and created loadCyclesFromFile

--- src/main/java/de/mdelab/predictor/evaluator/EvaluatorController.java
+++ src/main/java/de/mdelab/predictor/evaluator/EvaluatorController.java
@@ -7,39 +7,56 @@ import java.util.LinkedHashMap;
 import de.mdelab.predictor.loader.ReadWriteFile;
 
 /**
- * Compute different metrics
+ * Compute different ranking metrics for a set of cycles made available
  * @author Christian M. Adriano
  *
  */
 public class EvaluatorController {
 
-	public class Cycle{
-		String cycle;
-		LinkedHashMap<Integer,String> predictedMap;
-		LinkedHashMap<Integer,String> optimalMap;
-	}
-	
-	
-	public void run(Integer metricType, String path, String sourceFileName){
-		RankingMetric metric = RankingMetric.buildInstance(metricType);
+	/**
+	 * 
+	 * @param metricType see types in RankingMetric class
+	 * @param path
+	 * @param sourceFileName
+	 * @return the cycleMap with the data loaded
+	 */
+	public  HashMap<Integer,Cycle> loadCyclesFromFile(String path, String sourceFileName){
+		
 		ArrayList<String> buffer = ReadWriteFile.readToBuffer(path, sourceFileName);
 		
 		//Skip header
 		String header[] = buffer.get(0).split(",");
 		buffer.remove(0);
 		
-		HashMap<String, Cycle> cycleMap = new HashMap<String, Cycle>();
-		
-		String lineArray[] =  buffer.get(1).split(",");
-		int cycle = new Integer(lineArray[7]).intValue();
-		int previousCycle=cycle;
+		HashMap<Integer, Cycle> cycleMap = new HashMap<Integer, Cycle>();
 		
 		for(String line: buffer){
-					
-			while
-				
-				String lineArray[] =  line.split(",");
+			String lineArray[] = line.split(",");
+			Integer cycleKey = new Integer(lineArray[7]).intValue();
+		
+			Cycle cycle = cycleMap.get(cycleKey);
+			if(cycle==null){
+				cycle = new Cycle(cycleKey);
+			}
+			
+			cycleMap.put(cycleKey, cycle);
 		}
+		
+		return cycleMap;
+	}
+	
+	/**
+	 * 
+	 * @param cycleMap
+	 * @return the cycleMap with the metrics calculated for each cycle (see the HashMap inside the Class Cycle)
+	 */
+	public HashMap<Integer,Cycle> runMetrics(HashMap<Integer,Cycle> cycleMap){
+		
+		RankingMetric metric = RankingMetric.buildInstance(metricType);
+		
+		
+		
+		return cycleMap;
 		
 	}
 }
