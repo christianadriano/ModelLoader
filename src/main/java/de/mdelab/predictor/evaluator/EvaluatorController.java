5f91117fde302e578ac0d7e00e753ae53aec496b
Christian Medeiros Adriano <christian.adriano@gmail.com>
2018-01-11 20:46:04 +0100

    Entry point and saving to file

--- src/main/java/de/mdelab/predictor/evaluator/EvaluatorController.java
+++ src/main/java/de/mdelab/predictor/evaluator/EvaluatorController.java
@@ -3,6 +3,7 @@ package de.mdelab.predictor.evaluator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import de.mdelab.predictor.loader.ReadWriteFile;
 
@@ -20,43 +21,77 @@ public class EvaluatorController {
 	 * @param sourceFileName
 	 * @return the cycleMap with the data loaded
 	 */
-	public  HashMap<Integer,Cycle> loadCyclesFromFile(String path, String sourceFileName){
-		
+	public  LinkedHashMap<String, Cycle> loadCyclesFromFile(String path, String sourceFileName){
+
 		ArrayList<String> buffer = ReadWriteFile.readToBuffer(path, sourceFileName);
-		
+
 		//Skip header
 		String header[] = buffer.get(0).split(",");
 		buffer.remove(0);
-		
-		HashMap<Integer, Cycle> cycleMap = new HashMap<Integer, Cycle>();
-		
+
+		LinkedHashMap<String, Cycle> cycleMap = new LinkedHashMap<String, Cycle>();
+
 		for(String line: buffer){
 			String lineArray[] = line.split(",");
-			Integer cycleKey = new Integer(lineArray[7]).intValue();
-		
+			String cycleKey = lineArray[7];
+
 			Cycle cycle = cycleMap.get(cycleKey);
 			if(cycle==null){
 				cycle = new Cycle(cycleKey);
 			}
-			
+			cycle.addEntry(lineArray);
 			cycleMap.put(cycleKey, cycle);
 		}
-		
 		return cycleMap;
 	}
-	
+
 	/**
 	 * 
 	 * @param cycleMap
 	 * @return the cycleMap with the metrics calculated for each cycle (see the HashMap inside the Class Cycle)
 	 */
-	public HashMap<Integer,Cycle> runMetrics(HashMap<Integer,Cycle> cycleMap){
-		
-		RankingMetric metric = RankingMetric.buildInstance(metricType);
-		
-		
-		
+	public LinkedHashMap<String, Cycle>  computeMetrics(LinkedHashMap<String, Cycle>  cycleMap){
+
+		/** For each all cycle maps compute the ranking metrics */
+		for(Map.Entry<String, Cycle> entry : cycleMap.entrySet()){
+			Cycle cycle = entry.getValue();
+					
+			/** for each cycle map compute all metrics*/
+			for(String metricType:RankingMetric.metricsList){
+				RankingMetric metric = RankingMetric.buildInstance(metricType);
+				Double value = metric.compute(cycle.optimalRank, cycle.predictedRank);
+				cycle.metricTable.put(metricType, value);
+			}
+		}
 		return cycleMap;
-		
 	}
+
+
+	public void saveCycleMapToFile(LinkedHashMap<String, Cycle> cycleMap,String path, String destFileName){
+		
+		ArrayList<String> fileContent = new ArrayList<String>();
+		
+		Cycle cycle = cycleMap.entrySet().iterator().next().getValue();
+		fileContent.add(cycle.printHeader());
+		
+		/** For each all cycle maps compute the ranking metrics */
+		for(Map.Entry<String, Cycle> entry : cycleMap.entrySet()){
+			cycle = entry.getValue();
+			fileContent.add(cycle.printMetricValues());
+		}
+		
+		ReadWriteFile.writeBackToBuffer(fileContent, path, destFileName);
+	}
+
+	public static void main(String args[]){
+		 
+		EvaluatorController controller =  new EvaluatorController();
+		String path = "C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//data//Validation//";
+		String outcomeFile = "Combined_Evaluation_Metrics.csv";
+		String inputFile = "Combined_Evaluation.csv";
+		LinkedHashMap<String, Cycle> cycleMap = controller.loadCyclesFromFile(path, inputFile);		
+		cycleMap = controller.computeMetrics(cycleMap);
+		controller.saveCycleMapToFile(cycleMap,path,outcomeFile);
+	}
+	
 }
